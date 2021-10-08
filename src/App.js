3dd2facbb177a5bffec577d5831d85c198d7b442
Ashish <as7220004@gmail.com>
2021-10-08 13:10:16 +0530

    Complete code for Marketplace

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landingpage from './pages/Landingpage';
+import Dashboard from './pages/Dasboard';
+import Mainlayout from './components/Mainlayout';
+import Createsellable from './components/Createsellable';
+import Myassets from './components/Myassets';
+import CreatorDashboard from './components/CreatorDashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landingpage}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+          <Route exact path="/dashboard" component={Mainlayout} />
+          <Route exact path="/sellAssets" component={Createsellable}/>
+          <Route exact path="/myassets" component={Myassets} />
+          <Route exact path="/creatorDashboard" component={CreatorDashboard} />
+        </Switch>
+      </Router>
+      {/* <Landingpage /> */}
+
     </div>
   );
 }
